b884ac962c285c54f6b44a9ba8b9d9338a33627a
Vimer <vimer@iMac.local>
2023-01-15 19:45:53 +0800

    initial commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/glepnir/coman.nvim.git
+git clone https://github.com/glepnir/dashboard-nvim.git
+git clone https://github.com/glepnir/lspsaga.nvim.git
+git clone https://github.com/glepnir/mcc.nvim.git
+git clone https://github.com/glepnir/mutchar.nvim.git
+git clone https://github.com/glepnir/template.nvim.git
+git clone https://github.com/glepnir/zephyr-nvim.git
+git clone https://github.com/neovim/nvim-lspconfig.git
+# markdown surport
+# The binary will be installed under $HOME/.local/bin (make sure this folder is in your PATH).
+# git clone https://github.com/artempyanykh/marksman.git
+# cd marksman
+# make install
---
+++ update.sh
@@ -0,0 +1,21 @@
+# 更新neovim依赖
+echo "update coman"
+cd coman.nvim && git pull && cd ..
+
+echo "update dashboard-nvim"
+cd dashboard-nvim && git pull && cd ..
+
+echo "update lspsaga"
+cd lspsaga.nvim && git pull && cd ..
+
+echo "update mcc"
+cd mcc.nvim && git pull && cd ..
+
+echo "update template"
+cd template.nvim && git pull && cd ..
+
+echo "update zephyr.nvim"
+cd zephyr-nvim && git pull && cd ..
+
+echo "update mutchar.nvim"
+cd mutchar.nvim && git pull && cd ..
